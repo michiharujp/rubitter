f14adf81745b3f285cc2d70b76b1eda8d27a1812
michi <teketeke0349outlook.jp>
2017-10-28 18:49:07 +0900

    rebert gitignore

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
+
+  test "should get home" do
+    get home_path
+    assert_response :success
+  end
+
+  test "should get about" do
+    get about_path
+    assert_response :success
+  end
+
+end
